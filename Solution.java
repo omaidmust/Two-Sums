02accb4468193c91b515e3c496e0b37daf02b659
omaidmust <48129041+omaidmust@users.noreply.github.com>
2021-12-29 14:46:03 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 12/29/21 Omaid M
+ * 
+ */
+public class Solution {
+    
+        public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
+
+
+
